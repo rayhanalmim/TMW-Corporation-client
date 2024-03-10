e6bb54a1a5ace0f1ceaae36b84bd1861811d5f97
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-10 18:15:13 +0600

    updated nablink for office desk

--- src/Page/Dashboard/Agent/AgentMenu.jsx
+++ src/Page/Dashboard/Agent/AgentMenu.jsx
@@ -1,51 +1,51 @@
-import { NavLink } from "react-router-dom";
-import {  FaBeer,  FaShoppingCart, FaListUl, FaUsers } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {  FaBeer,  FaShoppingCart } from "react-icons/fa";
 import { FaShop } from "react-icons/fa6";
 
 const AgentMenu = () => {
   return (
     <div className="flex flex-col gap-4">
-      <NavLink to="/">
-          <button className="btn btn-primary text-white w-full hover: ">
+      <Link to="/">
+          <button className="btn bg-sky-800  hover:text-black text-white w-full ">
             <FaShoppingCart className="mr-2" />
             Home
           </button>
-        </NavLink>
+        </Link>
         
-        <NavLink to="/addToCard">
-          <button className="btn btn-primary text-white w-full ">
+        <Link to="/addToCard">
+          <button className="btn bg-sky-800  hover:text-black text-white w-full ">
             <FaShoppingCart className="mr-2" />
             DSR Order Request
           </button>
-        </NavLink>
+        </Link>
 
-        {/* <NavLink to="/sellView">
-          <li className="btn btn-primary text-white w-full">
+        {/* <Link to="/sellView">
+          <li className="btn bg-sky-800  hover:text-black text-white w-full">
             <FaListUl className="mr-2" />
             Sell List
           </li>
-        </NavLink>
+        </Link>
 
-        <NavLink to="/memberlist ">
-          <li className="btn btn-primary text-white w-full">
+        <Link to="/memberlist ">
+          <li className="btn bg-sky-800  hover:text-black text-white w-full">
             <FaUsers className="mr-2" />
             List of Due
           </li>
-        </NavLink> */}
+        </Link> */}
 
-        <NavLink to="/manageProduct">
-          <li className="btn btn-primary text-white w-full">
+        <Link to="/manageProduct">
+          <li className="btn bg-sky-800  hover:text-black text-white w-full">
             <FaBeer className="mr-2" />
             Product Management
           </li>
-        </NavLink>
+        </Link>
 
-        <NavLink to="/shop">
-          <li className="btn btn-primary text-white w-full">
+        <Link to="/shop">
+          <li className="btn bg-sky-800  hover:text-black text-white w-full">
             <FaShop className="mr-2" />
            Client Shop Management
           </li>
-        </NavLink>
+        </Link>
     </div>
   );
 };
