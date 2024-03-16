04cd33ccaa9ab14a32202f1997715f4659103044
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-16 16:08:53 +0600

    start working on due feature

--- src/Page/DSR/RequestDetails.jsx
+++ src/Page/DSR/RequestDetails.jsx
@@ -42,6 +42,10 @@ const RequestDetails = () => {
         });
     }
 
+    const handleDue = () =>{
+        
+    }
+
     return (
         <div className="container mx-auto my-8 text-white  p-2">
             <div className="flex flex-col gap-10 lg:flex-row-reverse w-full">
@@ -118,10 +122,9 @@ const RequestDetails = () => {
                     </div>
                     <div className="flex justify-end mt-6">
                         <div className="flex gap-4">
-                            <Link to={`/checkOut/${id}`}>
-                                <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">CheckOut</button>
-                            </Link>
-                            <button onClick={handleDelete} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Reject</button>
+                            <button type="button" onClick={handleDue} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Accept due</button>
+
+                            <button onClick={handleDelete} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">All Reject</button>
                         </div>
                     </div>
                 </div>
