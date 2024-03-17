5213bb4b0f43ba7a3ed5475801e7b705859e582d
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-17 16:49:01 +0600

    add logo in bill pdf

--- src/Page/Memo/Memo.jsx
+++ src/Page/Memo/Memo.jsx
@@ -28,8 +28,14 @@ const Invoice = () => {
 
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white  rounded-md">
+    <div className="max-w-4xl mx-auto p-6 bg-white  rounded-md relative">
+
+<div className="w-24 mx-auto block rounded-full  absolute top-5 right-10">
+        <img className="" src='https://i.ibb.co/Qrc1Vvq/Black-And-White-Modern-Vintage-Retro-Brand-Logo-3.png' />
+      </div>
       <div className="text-center">
+
+
         <h1 className="text-3xl text-red-600 font-black font-mono">TMW Corporation</h1>
         {/* <div className="flex justify-end">
           <p className="pr-32 mt-3 font-extrabold">T. Ch. No: </p>
@@ -61,42 +67,42 @@ const Invoice = () => {
 
 
       <div className="mt-6">
-  <div className="overflow-x-auto">
-    <table className="w-full table-auto  border-dotted border-4 border-collapse">
-      <thead>
-        <tr className="bg-gray-200">
-          <th className="py-2 border text-center">Product Name</th>
-          <th className="py-2 border text-center">Qty</th>
-          <th className="py-2 border text-center">Ctn</th>
-          <th className="py-2 border text-center">Price</th>
-          <th className="py-2 border text-center">Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {demoData?.requestedItems?.map((product, idx) => (
-          <tr key={idx}>
-            <td className="py-2 border-dotted border-4 text-center">{product?.product?.productName}</td>
-            <td className="py-2 border-dotted border-4 text-center">{product?.quantity}</td>
-            <td className="py-2 border-dotted border-4 text-center">{product?.product?.perCartonQuantity}</td>
-            <td className="py-2 border-dotted border-4 text-center">{product?.product?.productPrice}</td>
-          
-            <td className="py-2 border-dotted border-4 text-center">
-              {product?.product?.perCartonQuantity + product?.product?.productPrice}
-            </td>
-          </tr>
-        ))}
-        {/* Total row */}
-        <tr>
-          <td className="py-2 border-dotted border-l-white border-y-white border-4"></td>
-          <td colSpan="3" className="py-2 border-dotted border-4 font-bold text-center">Total:</td>
-          <td className="py-2 border-dotted border-4 text-center font-bold">
-            {demoData?.totalPrice}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
+        <div className="overflow-x-auto">
+          <table className="w-full table-auto  border-dotted border-4 border-collapse">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="py-2 border text-center">Product Name</th>
+                <th className="py-2 border text-center">Qty</th>
+                <th className="py-2 border text-center">Ctn</th>
+                <th className="py-2 border text-center">Price</th>
+                <th className="py-2 border text-center">Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {demoData?.requestedItems?.map((product, idx) => (
+                <tr key={idx}>
+                  <td className="py-2 border-dotted border-4 text-center">{product?.product?.productName}</td>
+                  <td className="py-2 border-dotted border-4 text-center">{product?.quantity}</td>
+                  <td className="py-2 border-dotted border-4 text-center">{product?.product?.perCartonQuantity}</td>
+                  <td className="py-2 border-dotted border-4 text-center">{product?.product?.productPrice}</td>
+
+                  <td className="py-2 border-dotted border-4 text-center">
+                    {product?.product?.perCartonQuantity + product?.product?.productPrice}
+                  </td>
+                </tr>
+              ))}
+              {/* Total row */}
+              <tr>
+                <td className="py-2 border-dotted border-l-white border-y-white border-4"></td>
+                <td colSpan="3" className="py-2 border-dotted border-4 font-bold text-center">Total:</td>
+                <td className="py-2 border-dotted border-4 text-center font-bold">
+                  {demoData?.totalPrice}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
 
 
 
@@ -115,10 +121,12 @@ const Invoice = () => {
           <p className="text-right">Total Due: {user?.totalDueAmmout} TK</p>
         )}
       </div> */}
-      <div className="flex justify-between mx-8 mt-24t">
-      <p className="text-left mt-4 border-t-2 border-dotted">Received by</p>
-      <p className="text-left mt-4 border-t-2 border-dotted">TMW Corporation</p>
+      <div className="flex justify-between mx-8 mt-24">
+        <p className="text-left mt-4 border-t-2 border-dotted">Received by</p>
+        <p className="text-left mt-4 border-t-2 border-dotted">TMW Corporation</p>
       </div>
+      
+
     </div>
   );
 };
