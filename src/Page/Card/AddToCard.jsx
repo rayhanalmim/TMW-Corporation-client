17712c5c7891ee9a75c2b87daf6d76614102c242
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-26 23:55:29 +0600

    add basic functionality for add product from admin end

--- src/Page/Card/AddToCard.jsx
+++ src/Page/Card/AddToCard.jsx
@@ -1,13 +1,17 @@
 import { FiShoppingCart } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const AddToCard = () => {
+  const location = useLocation();
+
+  // Check if the current path matches '/request'
+  const isRequestRoute = location.pathname === "/request";
 
   return (
     <div>
       <Link to="/request">
         <div className="cursor-pointer">
-          <FiShoppingCart className="text-4xl hover:text-5xl"></FiShoppingCart>
+          <FiShoppingCart className={`text-4xl ${isRequestRoute ? 'text-5xl' : ''} hover:text-5xl`} />
         </div>
       </Link>
     </div>
--- src/Page/Card/RequestPage.jsx
+++ src/Page/Card/RequestPage.jsx
@@ -1,9 +1,12 @@
+import Title from "../../Components/Shared/Title";
 
 
 const RequestPage = () => {
     return (
         <div>
-            <title title="hello"></title>
+            <div className="relative border-s-8 border-rose-500 ">
+            <h2 className="text-6xl font-bold z-20 text-white">Listed Items</h2>
+        </div>
             <h3 className="text-white">can i here </h3>
         </div>
     );
