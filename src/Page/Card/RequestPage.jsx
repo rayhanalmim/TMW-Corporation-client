5c247d2ae9872024d5025aacd1a62fbb5857d5ba
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-27 10:59:53 +0600

    start working add desk requ

--- src/Page/Card/RequestPage.jsx
+++ src/Page/Card/RequestPage.jsx
@@ -5,9 +5,9 @@ const RequestPage = () => {
     return (
         <div>
             <div className="relative border-s-8 border-rose-500 ">
-            <h2 className="text-6xl font-bold z-20 text-white">Listed Items</h2>
-        </div>
-            <h3 className="text-white">can i here </h3>
+                <h2 className="text-6xl font-bold z-20 text-white">Listed Items</h2>
+            </div>
+            <div></div>
         </div>
     );
 };
