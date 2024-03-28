6d000d8b980828182a69f0fd2978f946dc540b7e
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-28 22:57:48 +0600

    working at layout part in shop due pay

--- src/Page/Shop/SingleShopInfo.jsx
+++ src/Page/Shop/SingleShopInfo.jsx
@@ -46,7 +46,7 @@ const SingleShopInfo = () => {
         e.target.paid.value = '';
         refetch();
       }
-    }else{
+    } else {
       const swalWithBootstrapButtons = Swal.mixin({
         customClass: {
           confirmButton: "btn btn-success",
@@ -153,7 +153,24 @@ const SingleShopInfo = () => {
                     <td>{product?.orderNo}</td>
                     <td>{product?.orderDate}</td>
 
-                    <td>{product?.orderTime}</td>
+                    <td>
+                      <div className="flex gap-3">
+                            <input
+                              id="paidAmount"
+                              onChange={(e) => setPaid(e.target.value)}
+                              type="number"
+                              name="paid"
+                              className="w-1/2 bg-white text-black px-4 py-2.5 rounded-md dark-border-gray-700 focus:dark-border-violet-400"
+                            />
+                          <button
+                            type="submit"
+                            className="relative btn-xs inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                              Add
+                            </span>
+                          </button>
+                      </div>
+                    </td>
                     <td>{product?.dsrInfo?.displayName}</td>
                     <td>{product?.due}</td>
                     <td className="flex justify-center">
