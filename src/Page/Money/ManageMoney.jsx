46f8fb64ca589451f3886139e0e660ca8a35c48d
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-28 22:40:42 +0600

    remove unnessesary code

--- src/Page/Money/ManageMoney.jsx
+++ src/Page/Money/ManageMoney.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../Hook/useAxiosSecure";
