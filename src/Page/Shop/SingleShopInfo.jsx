e3992efd9c1443d23993b1cc7a7b1c7333ef998c
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-29 12:48:28 +0600

    remove some unnesessary code

--- src/Page/Shop/SingleShopInfo.jsx
+++ src/Page/Shop/SingleShopInfo.jsx
@@ -4,7 +4,6 @@ import { useQuery } from "@tanstack/react-query";
 import { FaDownload } from "react-icons/fa";
 import { useState } from "react";
 import Swal from "sweetalert2";
-import { TbH3 } from "react-icons/tb";
 
 const SingleShopInfo = () => {
   const { id } = useParams();
