59132eff6b0947c0e6a909bdf8fc45e81974877a
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-31 13:43:18 +0600

    add some modification on mobile responsive

--- src/Components/Shared/Title.jsx
+++ src/Components/Shared/Title.jsx
@@ -2,7 +2,7 @@
 const Title = ({title}) => {
     return (
         <div className="relative border-s-8 border-orange-500 ">
-            <h2 className="text-6xl font-bold z-20">{title}</h2>
+            <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold z-20">{title}</h2>
         </div>
     );
 };
--- src/Page/Dashboard/Dashboard.jsx
+++ src/Page/Dashboard/Dashboard.jsx
@@ -14,8 +14,8 @@ const Dashboard = () => {
     <div className="bg-sky-900 min-h-screen">
       <Header />
       <div className="flex flex-col lg:flex-row w-full  mx-auto gap-5 relative">
-        <div className="w-full lg:w-1/3 p-4 ">
-          <div className="justify-around  top-6 sticky p-6 lg:p-8 gap-9 text-center rounded-lg my-8 bg-sky-800 w-full mx-auto hidden lg:block">
+        <div className="w-full lg:w-1/3 p-4 hidden lg:block">
+          <div className="justify-around  top-6 sticky p-6 lg:p-8 gap-9 text-center rounded-lg my-8 bg-sky-800 w-full mx-auto ">
             <div>
               {isAdmin ? (
                 <AdminMenu />
@@ -27,8 +27,8 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        <div className="w-full lg:w-2/3 p-4 pt-0 lg:pt-44 sticky top-0">
-          <div className="w-full justify-around p-1 lg:p-8 gap-9 text-center rounded-lg lg:-mt-32 bg-sky-800 ">
+        <div className="w-full lg:w-2/3 p-4 pt-5 lg:pt-44 sticky top-0">
+          <div className="w-full justify-around lg:p-8 gap-9 text-center rounded-lg lg:-mt-32 bg-sky-800 ">
             <Outlet />
           </div>
         </div>
