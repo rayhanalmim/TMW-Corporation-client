9e6a6039cd8dad3af7ef702e3c21933119123869
rayhanalmim <rayhanalmim1@gmail.com>
2024-04-03 23:38:28 +0600

    add bill route for desk admin

--- src/Page/Dashboard/Agent/AgentMenu.jsx
+++ src/Page/Dashboard/Agent/AgentMenu.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {  FaBeer,  FaShoppingCart } from "react-icons/fa";
+import { FaBeer, FaShoppingCart, FaListUl } from "react-icons/fa";
 import { FaShop } from "react-icons/fa6";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../Hook/useAxiosSecure";
@@ -28,36 +28,43 @@ const AgentMenu = () => {
     // Clean up the interval on component unmount
     return () => clearInterval(interval);
   }, []); // Empty dependency array ensures the effect runs only once on mount
-  
+
   return (
     <div className="flex flex-col gap-4">
       <Link to="/">
-          <button className="btn bg-sky-800  hover:text-black text-white w-full ">
-            <FaShoppingCart className="mr-2" />
-            Home
-          </button>
-        </Link>
-        
-        <Link to="/dsr">
-          <button className="btn bg-sky-800  hover:text-black text-white w-full ">
-            <FaShoppingCart className="mr-2" />
-            DSR Order Request ({infos.length})
-          </button>
-        </Link>
+        <button className="btn bg-sky-800  hover:text-black text-white w-full ">
+          <FaShoppingCart className="mr-2" />
+          Home
+        </button>
+      </Link>
 
-        <Link to="/manageProduct">
-          <li className="btn bg-sky-800  hover:text-black text-white w-full">
-            <FaBeer className="mr-2" />
-            Product Management
+      <Link to="/dsr">
+        <button className="btn bg-sky-800  hover:text-black text-white w-full ">
+          <FaShoppingCart className="mr-2" />
+          DSR Order Request ({infos.length})
+        </button>
+      </Link>
+
+      <Link to="/sellView">
+          <li className="btn bg-sky-800 text-white w-full">
+            <FaListUl className="mr-2" />
+            Billing Management
           </li>
         </Link>
 
-        <Link to="/shop">
-          <li className="btn bg-sky-800  hover:text-black text-white w-full">
-            <FaShop className="mr-2" />
-           Client Shop Management
-          </li>
-        </Link>
+      <Link to="/manageProduct">
+        <li className="btn bg-sky-800  hover:text-black text-white w-full">
+          <FaBeer className="mr-2" />
+          Product Management
+        </li>
+      </Link>
+
+      <Link to="/shop">
+        <li className="btn bg-sky-800  hover:text-black text-white w-full">
+          <FaShop className="mr-2" />
+          Client Shop Management
+        </li>
+      </Link>
     </div>
   );
 };
