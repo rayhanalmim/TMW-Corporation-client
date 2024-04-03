86f6be66d5288586544a8b230b4ac0c10cc107c7
rayhanalmim <rayhanalmim1@gmail.com>
2024-04-03 22:57:12 +0600

    working on ui on sell analysis

--- src/Page/Analysis/Analysis.jsx
+++ src/Page/Analysis/Analysis.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hook/useAxiosSecure";
 import Marquee from "react-fast-marquee";
 
+
 const Analysis = () => {
   const axiosSecure = useAxiosSecure();
 
@@ -17,7 +18,7 @@ const Analysis = () => {
 
   const formatMonth = (monthString) => {
     const date = new Date(monthString);
-    const month = date.toLocaleString('default', { month: 'long' });
+    const month = date.toLocaleString("default", { month: "long" });
     const year = date.getFullYear();
     return `${month} ${year}`;
   };
@@ -25,16 +26,18 @@ const Analysis = () => {
   const formatDateForDaily = (dateString) => {
     const date = new Date(dateString);
     const day = date.getDate();
-    const month = date.toLocaleString('default', { month: 'long' });
+    const month = date.toLocaleString("default", { month: "long" });
     const year = date.getFullYear();
     return `${day} ${month} ${year}`;
   };
 
   // Calculate monthly sell data only if monthlySellAmount is defined
-  const monthlySellData = monthlySellAmount ? monthlySellAmount.map(monthData => ({
-    month: monthData.month,
-    totalAmmount: monthData.totalAmmount
-  })) : [];
+  const monthlySellData = monthlySellAmount
+    ? monthlySellAmount.map((monthData) => ({
+      month: monthData.month,
+      totalAmmount: monthData.totalAmmount,
+    }))
+    : [];
 
   // Calculate each month's sell value
   for (let i = 1; i < monthlySellData.length; i++) {
@@ -42,65 +45,132 @@ const Analysis = () => {
   }
 
   return (
-    <div className="p-4 text-white">
-      <Marquee speed={100}>
-        <h2 className="text-2xl font-bold ">Sell History</h2>
-      </Marquee>
+    <div className="p-4 text-gray-800 bg-gradient-to-r from-sky-600 via-indigo-500 to-blue-500 rounded-lg shadow-md relative">
+      <div className="relative">
+        <div className="absolute top-0 left-0 w-full h-full  opacity-80 z-0"></div>
+        <div className="relative z-10">
+          <Marquee speed={100}>
+            <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white text-center mb-4">Sell History</h2>
+          </Marquee>
+        </div>
+      </div>
 
-      <hr />
 
-      <div className="mb-8 pt-6">
-        <h3 className="text-lg font-semibold mb-2">Total Sell Amount:</h3>
-        <table className="table-auto border-collapse w-full">
-          <tbody>
-            <tr>
-              <td className="border px-4 py-2">{totalSellAmount}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="absolute right-0 top-0">
+      </div>
+
+      <hr className="mb-6" />
+
+      <div className="mb-8">
+        <div className="flex justify-center">
+          <div className="flex  mb-2 relative">
+            <h3 className="text-2xl font-bold text-gray-800 ml-8">Total Sell Amount:</h3>
+            <div className="absolute -left-2 -top-1 bg-rose-400 w-8 h-8 rounded-full animate-ping"></div>
+            <div className="absolute -left-2 -top-1 bg-rose-400 w-8 h-8 rounded-full animate-pulse"></div>
+          </div>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="table-auto border-collapse w-full">
+            <tbody>
+              <tr className="bg-white">
+                <td className="border px-4 py-2">Total</td>
+                <td className="border px-4 py-2">{totalSellAmount}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
 
       <div className="mb-8">
-        <h3 className="text-lg font-semibold m-2">Daily Sell Amount:</h3>
-        <table className="table-auto border-collapse w-full">
-          <tbody>
-            {dailySellAmmount?.slice(0, 30).reverse().map(dayData => (
-              <tr key={dayData.day}>
-                <td className="border px-4 py-2">{formatDateForDaily(dayData.day)}</td>
-                <td className="border px-4 py-2">{dayData.totalAmmount}</td>
+        <div className="flex justify-center">
+          <div className="flex  mb-2 relative">
+            <h3 className="text-2xl font-bold text-gray-800 ml-8">Daily Sell Amount:</h3>
+            <div className="absolute -left-2 -top-1 bg-blue-400 w-8 h-8 rounded-full animate-ping"></div>
+            <div className="absolute -left-2 -top-1 bg-blue-400 w-8 h-8 rounded-full animate-pulse"></div>
+          </div>
+        </div>
+
+
+        <div className="overflow-x-auto">
+          <table className="table-auto border-collapse w-full">
+            <thead>
+              <tr className="bg-white">
+                <th className="border px-4 py-2">Date</th>
+                <th className="border px-4 py-2">Amount</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {dailySellAmmount?.slice(0, 30).reverse().map((dayData, index) => (
+                <tr key={dayData.day}>
+                  <td className={`border px-4 py-2 ${index % 2 !== 0 ? 'bg-white' : 'text-gray-200'}`}>{formatDateForDaily(dayData.day)}</td>
+                  <td className={`border px-4 py-2 ${index % 2 !== 0 ? 'bg-white' : 'text-gray-200'}`}>{dayData.totalAmmount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
 
-      <div className="mb-8">
-        <h3 className="text-lg font-semibold mb-2">Monthly Sell Amount:</h3>
-        <table className="table-auto border-collapse w-full">
-          <tbody>
-            {monthlySellData.map(monthData => (
-              <tr key={monthData.month}>
-                <td className="border px-4 py-2">{formatMonth(monthData.month)}</td>
-                <td className="border px-4 py-2">{monthData.totalAmmount}</td>
+
+      <div className="mb-8 ">
+        <div className="flex justify-center">
+          <div className="flex  mb-2 relative">
+            <h3 className="text-2xl font-bold text-gray-800 ml-8">Monthly Sell Amount</h3>
+            <div className="absolute -left-2 -top-1 bg-yellow-400 w-8 h-8 rounded-full animate-ping"></div>
+            <div className="absolute -left-2 -top-1 bg-yellow-400 w-8 h-8 rounded-full animate-pulse"></div>
+          </div>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="table-auto border-collapse w-full">
+            <thead>
+              <tr className="bg-white">
+                <th className="border px-4 py-2">Month</th>
+                <th className="border px-4 py-2">Amount</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {monthlySellData.map((monthData, index) => (
+                <tr key={monthData.month}>
+                  <td className={`border px-4 py-2 ${index % 2 !== 0 ? 'bg-white' : 'text-gray-200'}`}>{formatMonth(monthData.month)}</td>
+                  <td className={`border px-4 py-2 ${index % 2 !== 0 ? 'bg-white' : 'text-gray-200'}`}>{monthData.totalAmmount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
 
+
       <div className="mb-8">
-        <h3 className="text-lg font-semibold mb-2">Yearly Sell Amount:</h3>
-        <table className="table-auto border-collapse w-full">
-          <tbody>
-            {yearlySellAmount?.map(yearData => (
-              <tr key={yearData.year}>
-                <td className="border px-4 py-2">{yearData.year}</td>
-                <td className="border px-4 py-2">{yearData.totalAmount}</td>
+        <div className="flex justify-center">
+          <div className="flex  mb-2 relative">
+            <h3 className="text-2xl font-bold text-gray-800 ml-8">Yearly Sell Amount:</h3>
+            <div className="absolute -left-2 -top-1 bg-green-400 w-8 h-8 rounded-full animate-ping"></div>
+            <div className="absolute -left-2 -top-1 bg-green-400 w-8 h-8 rounded-full animate-pulse"></div>
+          </div>
+        </div>
+        <div className="overflow-x-auto">
+          <table className="table-auto border-collapse w-full">
+            <thead>
+              <tr className="bg-white">
+                <th className="border px-4 py-2">Year</th>
+                <th className="border px-4 py-2">Amount</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {yearlySellAmount?.map((yearData, index) => (
+                <tr key={yearData.year}>
+                  <td className={`border px-4 py-2 ${index % 2 !== 0 ? 'bg-white' : 'text-gray-200'}`}>{yearData.year}</td>
+                  <td className={`border px-4 py-2 ${index % 2 !== 0 ? 'bg-white' : 'text-gray-200'}`}>{yearData.totalAmount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
+
+
     </div>
   );
 };
